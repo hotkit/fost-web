e44a515039eed97989a947f3be54d2b9fcaac71c
Kirit Sælensminde <k@kirit.com>
2011-10-01 15:17:10 +0700

    Sketched out a simple view class.

--- kirit-com/Cpp/include/urlhandler.hpp
+++ kirit-com/Cpp/include/urlhandler.hpp
@@ -10,11 +10,25 @@
 #include <fost/http.server.hpp>
 
 
+/// URL routing for requests
 namespace urlhandler {
 
 
     /// The prime routing for web sites
     bool service( fostlib::http::server::request &req );
+
+    /// A view class
+    class view {
+        protected:
+            /// The name of the configuration that the handler should tie to
+            view(const fostlib::string &name);
+            /// Allow sub-classing to work properly
+            virtual ~view();
+        public:
+            /// Handle the request.
+            virtual void operator () (fostlib::http::server::request &,
+                const fostlib::host &) const = 0;
+    };
 
 
 }
