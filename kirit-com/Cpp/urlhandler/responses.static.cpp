43c3ac733820c540645f2e30a751a2b9d4a1cc88
Kirit Sælensminde <k@kirit.com>
2011-10-02 09:11:51 +0700

    Now uses the file specification in the request to determine the file to serve.

--- kirit-com/Cpp/urlhandler/responses.static.cpp
+++ kirit-com/Cpp/urlhandler/responses.static.cpp
@@ -19,11 +19,16 @@ const class response_static : public urlhandler::view {
         std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
             const fostlib::json &configuration,
             fostlib::http::server::request &req,
-            const fostlib::host &
+            const fostlib::host &host
         ) const {
             boost::filesystem::wpath root(
                 fostlib::coerce<boost::filesystem::wpath>(configuration["root"]));
-            boost::filesystem::wpath filename = root / L"index.html";
+            boost::filesystem::wpath filename = root /
+                fostlib::coerce<boost::filesystem::wpath>(req.file_spec());
+            if ( boost::filesystem::is_directory(filename) )
+                filename /= L"index.html";
+            if ( !boost::filesystem::exists(filename) )
+                return urlhandler::response_404(fostlib::json(), req, host);
             boost::shared_ptr<fostlib::mime> response(
                     new fostlib::file_body(filename, fostlib::mime::mime_headers(),
                         urlhandler::mime_type(filename)));
