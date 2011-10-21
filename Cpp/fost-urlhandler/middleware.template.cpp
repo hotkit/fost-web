65d7edacd25bcea843b55643735bf1ebe3e5771d
Kirit Sælensminde <k@kirit.com>
2011-10-21 11:57:26 +0700

    A very simple implementation of the sort of functionality we need.

--- Cpp/fost-urlhandler/middleware.template.cpp
+++ Cpp/fost-urlhandler/middleware.template.cpp
@@ -18,13 +18,38 @@ const class middleware_template : public fostlib::urlhandler::view {
         std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
             const fostlib::json &configuration,
             fostlib::http::server::request &req,
-            const fostlib::host &
+            const fostlib::host &h
         ) const {
-            boost::shared_ptr<fostlib::mime> response(
-                    new fostlib::text_body(
-                        L"<html><body>Template</body></html>",
-                        fostlib::mime::mime_headers(), L"text/html" ));
-            return std::make_pair(response, 200);
+            fostlib::string view(fostlib::coerce<fostlib::string>(
+                configuration["view"]));
+            std::pair<fostlib::string, fostlib::json> view_fn = view::find_view(view);
+            std::pair<boost::shared_ptr<fostlib::mime>, int>  wrapped(
+                view_for(view_fn.first)(view_fn.second, req, h));
+            if ( wrapped.second == 200 &&
+                    wrapped.first->headers()["Content-Type"].value() == "text/html" ) {
+                fostlib::string skin(
+                    fostlib::utf::load_file(
+                        fostlib::coerce<boost::filesystem::wpath>(configuration["template"])));
+                fostlib::string content(
+                    fostlib::coerce<fostlib::string>(*wrapped.first));
+
+                std::pair<fostlib::string, fostlib::nullable<fostlib::string> > title(
+                    fostlib::crack(content, "<title>", "</title>"));
+                if ( !title.second.isnull() )
+                    skin = replaceAll(skin, "{{ element:title }}",
+                        L"<title>" + title.second.value() + L"</title>");
+
+                std::pair<fostlib::string, fostlib::nullable<fostlib::string> > inner(
+                    fostlib::crack(content, "<body>", "</body>"));
+                if ( !inner.second.isnull() )
+                    skin = replaceAll(skin, "{{ content:body }}", inner.second.value());
+
+                boost::shared_ptr<fostlib::mime> response(
+                        new fostlib::text_body(skin,
+                            fostlib::mime::mime_headers(), L"text/html" ));
+                return std::make_pair(response, 200);
+            } else
+                return wrapped;
         }
 } c_middleware_template;
 
