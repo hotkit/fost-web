b1a67f519777ef40af3074ac1748a3c49f63bae7
Kirit Sælensminde <k@kirit.com>
2014-05-09 13:39:39 +0700

    We now require fost-internet.

--- Cpp/proxy-lib/proxy-text-tests.cpp
+++ Cpp/proxy-lib/proxy-text-tests.cpp
@@ -1,5 +1,13 @@
 #include <proxy/proxy-lib>
+#include <fost/http>
+#include <fost/http.server.hpp>
 #include <fost/test>
 
 
 FSL_TEST_SUITE(proxy_lib);
+
+
+FSL_TEST_FUNCTION(can_see_config) {
+    fostlib::http::server::request req("GET", "/_config");
+}
+
