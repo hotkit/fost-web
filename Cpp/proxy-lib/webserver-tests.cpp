9f8897878ceb02b9959affebaa4b848a2e0f2ad9
Kirit Sælensminde <k@kirit.com>
2014-05-10 15:03:43 +0700

    Start up the web server.

--- Cpp/proxy-lib/webserver-tests.cpp
+++ Cpp/proxy-lib/webserver-tests.cpp
@@ -1,3 +1,4 @@
+#include <fost/http>
 #include <fost/test>
 #include <proxy/webserver.hpp>
 
@@ -6,7 +7,13 @@ FSL_TEST_SUITE(webserver);
 
 
 FSL_TEST_FUNCTION(starts_and_stops) {
+    fostlib::http::user_agent ua;
+
+    FSL_CHECK_EXCEPTION(
+        ua.get(fostlib::url("http://localhost:2555")),
+        fostlib::exceptions::connect_failure&);
     proxy::start("/");
+    FSL_CHECK_NOTHROW(ua.get(fostlib::url("http://localhost:2555")));
     proxy::stop();
     proxy::wait();
 }
--- Cpp/proxy-lib/webserver.cpp
+++ Cpp/proxy-lib/webserver.cpp
@@ -1,8 +1,31 @@
+#include <fost/internet>
+#include <fost/http.server.hpp>
 #include <fost/log>
 #include <proxy/webserver.hpp>
+#include <proxy/views.hpp>
+
+
+namespace {
+    bool service(fostlib::http::server::request &req) {
+        std::pair<boost::shared_ptr<fostlib::mime>, int > response =
+            proxy::view::c_proxy(fostlib::json(),
+                fostlib::coerce<fostlib::string>(req.file_spec()),
+                req, fostlib::host());
+        req(*response.first, response.second);
+        return true;
+    }
+
+    fostlib::worker g_server;
+    boost::shared_ptr< fostlib::detail::future_result< void > > g_running;
+}
 
 
 void proxy::start(const boost::filesystem::wpath &root) {
+    g_running = g_server([]() {
+        fostlib::http::server server(fostlib::host(0), 2555);
+        server(service);
+    });
+    sleep(1);
 }
 
 
