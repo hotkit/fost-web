67a531e0f5700e1e74df568a3ffb5f8b94da198f
Kirit Sælensminde <k@kirit.com>
2014-05-15 14:02:02 +0700

    Make sure the request that's meant to work actually does.

--- Cpp/proxy-lib/proxy-view-tests.cpp
+++ Cpp/proxy-lib/proxy-view-tests.cpp
@@ -10,10 +10,11 @@ FSL_TEST_SUITE(proxy);
 FSL_TEST_FUNCTION(can_get_page) {
     fostlib::http::server::request req(
         "GET", "/images/league_images/la_liga.jpg");
-    std::pair<boost::shared_ptr<fostlib::mime>, int > response =
-        proxy::view::c_proxy(
+    std::pair<boost::shared_ptr<fostlib::mime>, int > response;
+    FSL_CHECK_NOTHROW(
+        response = proxy::view::c_proxy(
             fostlib::json(), "/images/league_images/la_liga.jpg",
-            req, fostlib::host());
+            req, fostlib::host()));
     FSL_CHECK_EQ(response.second, 200);
     FSL_CHECK_EQ(
         response.first->headers()["content-type"].value(),
