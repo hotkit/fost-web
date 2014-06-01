f31e1b73afe6dc3804db3f51c93356d49d4fd00a
Kirit Sælensminde <k@kirit.com>
2014-06-01 12:53:42 +0700

    Sketch out fetching of the overall stats mechanism.

--- Cpp/proxy-lib/stats.cpp
+++ Cpp/proxy-lib/stats.cpp
@@ -1,34 +1,62 @@
 #include <proxy/stats.hpp>
-#include <fost/counter>
+#include <fost/datetime>
 #include <fost/threading>
 
 
 namespace {
 
 
-    /// The overall number of requests made
-    fostlib::counter g_requests;
-
     /// A shared mutex (multiple readers, one writer)
     boost::shared_mutex g_mutex;
+    /// The number of requests so far
+    int64_t g_requests = 0;
 
     /// Statistics for a given resource
     struct resource {
+        boost::mutex mutex;
         fostlib::url address;
-        fostlib::counter origin_requests, origin_failures,
+        fostlib::timestamp accessed;
+        int64_t requests,
+            origin_requests, origin_failures,
             cache_hits, cache_misses;
+        resource(const fostlib::url &a)
+        : address(a), accessed(fostlib::timestamp::now()),
+                requests(1), origin_requests(0), origin_failures(0),
+                cache_hits(0), cache_misses(0) {
+        }
     };
 
     /// The store
     typedef std::map<fostlib::string, std::unique_ptr<resource>> stats_type;
+
+    stats_type &g_store() {
+        static stats_type s;
+        return s;
+    }
 
 
 }
 
 
 void proxy::stats::request(
-    const fostlib::string &hash, const fostlib::url &resource
+    const fostlib::string &hash, const fostlib::url &uri
 ) {
-    ++g_requests;
+    boost::upgrade_lock<boost::shared_mutex> read_lock(g_mutex);
+    stats_type::iterator location(g_store().find(hash));
+    if ( location == g_store().end() ) {
+        /// Resource we've not seen before
+        boost::upgrade_to_unique_lock<boost::shared_mutex>
+            write_lock(read_lock);
+        g_store().insert(std::make_pair(
+            hash, std::unique_ptr<resource>(new resource(uri))));
+        ++g_requests;
+    } else {
+        /// Already have the resource
+        boost::mutex::scoped_lock lock(location->second->mutex);
+        ++location->second->requests;
+        boost::upgrade_to_unique_lock<boost::shared_mutex>
+            write_lock(read_lock);
+        ++g_requests;
+    }
 }
 
