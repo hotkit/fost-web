eb3ead95f486fa1803b08938b6be6992f90b5534
Kirit Sælensminde <k@kirit.com>
2014-06-01 12:44:48 +0700

    We are going to need to be able to load stats from a JSON file.

--- Cpp/include/proxy/stats.hpp
+++ Cpp/include/proxy/stats.hpp
@@ -23,6 +23,8 @@ namespace proxy {
         /// Store a cache miss against the requested resource
         void miss(const fostlib::string &hash, const fostlib::url &resource);
 
+        /// Load previous stats from the JSON structure
+        void load(const fostlib::json &);
         /// Return a JSON structure describing the current stats
         fostlib::json current();
 
