46a7d2ae86e3d9875da61fa87fa2ec8fb88c0c4b
Kirit Sælensminde <k@kirit.com>
2014-06-01 14:26:49 +0700

    More tightly connect the implementations we need.

--- Cpp/proxy-lib/stats.cpp
+++ Cpp/proxy-lib/stats.cpp
@@ -6,29 +6,36 @@
 namespace {
 
 
-    /// A shared mutex (multiple readers, one writer)
-    boost::shared_mutex g_mutex;
-    /// The number of requests so far
-    int64_t g_requests = 0;
-
-    /// Statistics for a given resource
-    struct resource {
+    /// Overall aggregate statistics
+    struct statistics {
         boost::mutex mutex;
-        fostlib::url address;
         fostlib::timestamp accessed;
         int64_t requests,
             origin_requests, origin_failures,
             cache_hits, cache_misses;
-        resource(const fostlib::url &a)
-        : address(a), accessed(fostlib::timestamp::now()),
-                requests(1), origin_requests(0), origin_failures(0),
+        statistics()
+        : accessed(), requests(0),
+                origin_requests(0), origin_failures(0),
                 cache_hits(0), cache_misses(0) {
+        }
+    } g_aggregate;
+
+    /// Statistics for a given resource
+    struct resource : public statistics {
+        fostlib::url address;
+        resource(const fostlib::url &a)
+        : address(a) {
+            accessed = fostlib::timestamp::now();
+            requests = 1;
         }
     };
 
-    /// The store
+    /// The store type
     typedef std::map<fostlib::string, std::unique_ptr<resource>> stats_type;
 
+    /// A shared mutex (multiple readers, one writer)
+    boost::shared_mutex g_mutex;
+    /// The store handled by the mutex
     stats_type &g_store() {
         static stats_type s;
         return s;
@@ -47,16 +54,16 @@ void proxy::stats::request(
         /// Resource we've not seen before
         boost::upgrade_to_unique_lock<boost::shared_mutex>
             write_lock(read_lock);
-        g_store().insert(std::make_pair(
-            hash, std::unique_ptr<resource>(new resource(uri))));
-        ++g_requests;
-    } else {
-        /// Already have the resource
+        location = g_store().insert(std::make_pair(
+            hash, std::unique_ptr<resource>(new resource(uri)))).first;
+    }
+    {
         boost::mutex::scoped_lock lock(location->second->mutex);
         ++location->second->requests;
-        boost::upgrade_to_unique_lock<boost::shared_mutex>
-            write_lock(read_lock);
-        ++g_requests;
+    }
+    {
+        boost::mutex::scoped_lock lock(g_aggregate.mutex);
+        ++g_aggregate.requests;
     }
 }
 
