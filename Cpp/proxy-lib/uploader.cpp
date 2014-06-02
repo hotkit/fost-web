db05f9947acc0bc4618af4df6b89cdf6bcf264fb
Kirit Sælensminde <k@kirit.com>
2014-06-02 16:16:01 +0700

    Add an initial uploader to send data to Swift/S3.

---
+++ Cpp/proxy-lib/uploader.cpp
@@ -0,0 +1,51 @@
+#include <fost/aws>
+#include <fost/jsondb>
+#include <varanus/acanthurus.hpp>
+#include <varanus/obor.hpp>
+
+
+namespace {
+
+
+    const fostlib::setting<fostlib::string> c_account(
+        "proxy-lib/uploader.cpp",
+        fostlib::aws::s3::bucket::s_account_name, "wos");
+    const fostlib::setting<fostlib::string> c_bucket(
+        "proxy-lib/uploader.cpp",
+        "Uploader", "bucket", "fbwc2014", true);
+
+
+    fostlib::json upload_host_db(const fostlib::json &config) {
+        fostlib::aws::s3::bucket bucket(
+            fostlib::coerce<fostlib::ascii_printable_string>(
+                c_bucket.value()));
+
+        fostlib::jsondb::local acanthurus(varanus::acanthurus_config());
+        fostlib::string this_host(
+            fostlib::coerce<fostlib::string>(
+                acanthurus["hostname"]));
+
+        fostlib::string host(fostlib::coerce<
+            fostlib::nullable<fostlib::string>>(config["hostname"])
+                .value("localhost"));
+        varanus::db hostdb(varanus::host_data(host));
+        fostlib::jsondb::local hostdata(hostdb());
+
+        fostlib::string content(fostlib::json::unparse(
+            hostdata[fostlib::jcursor()], true));
+        boost::filesystem::wpath to(
+            boost::filesystem::wpath("/app") /
+            fostlib::coerce<boost::filesystem::wpath>(this_host) /
+            fostlib::coerce<boost::filesystem::wpath>(host) /
+            fostlib::coerce<boost::filesystem::wpath>(
+                fostlib::timestamp::now()));
+        to.replace_extension(".json");
+        bucket.put(content, to);
+
+        return fostlib::json();
+    }
+    const varanus::obor c_statistics("proxy.uploader.db.host", upload_host_db);
+
+
+}
+
