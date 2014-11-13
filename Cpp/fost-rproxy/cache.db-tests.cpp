7d70e4a0c48106aa1678ebd034ff749a3578accb
Kirit Sælensminde <k@kirit.com>
2014-11-13 10:05:55 +0700

    Rename the cache header and start to fix up tests.

--- Cpp/fost-rproxy/cache.db-tests.cpp
+++ Cpp/fost-rproxy/cache.db-tests.cpp
@@ -14,33 +14,33 @@ FSL_TEST_SUITE(cache_db);
 
 
 FSL_TEST_FUNCTION(empty_db) {
-    proxy::flush_cache();
-    std::shared_ptr<fostlib::jsondb> db(proxy::cache_db());
+    fostlib::flush_cache();
+    boost::shared_ptr<fostlib::jsondb> db(fostlib::cache_db());
     fostlib::jsondb::local trans(*db);
     FSL_CHECK_EQ(trans["file-db"],
         fostlib::json(fostlib::json::object_t()));
-    FSL_CHECK_EQ(db, proxy::cache_db());
+    FSL_CHECK_EQ(db, fostlib::cache_db());
 }
 
 
 FSL_TEST_FUNCTION(save_response) {
-    proxy::flush_cache();
+    fostlib::flush_cache();
     fostlib::http::server::request req("GET", "/");
     fostlib::http::user_agent::response r("GET",
         fostlib::url("http://example.com/"), 200,
         boost::make_shared<fostlib::binary_body>());
     FSL_CHECK_EQ(
-        proxy::save_entry(req, r),
+        fostlib::save_entry(req, r),
         boost::filesystem::wpath("d3") /
             "c8eae015367cfdcd581ddbef8fa58f"
             "-d41d8cd98f00b204e9800998ecf8427e");
-    std::shared_ptr<fostlib::jsondb> db(proxy::cache_db());
+    boost::shared_ptr<fostlib::jsondb> db(fostlib::cache_db());
     fostlib::jsondb::local trans(*db);
     FSL_CHECK_EQ(trans["file-db"]["d3"]["db"],
         fostlib::coerce<fostlib::json>(
             boost::filesystem::wpath("d3") / "file-db.json"));
     FSL_CHECK_EQ(
-        proxy::save_entry(req, r),
+        fostlib::save_entry(req, r),
         boost::filesystem::wpath("d3") /
             "c8eae015367cfdcd581ddbef8fa58f"
             "-d41d8cd98f00b204e9800998ecf8427e");
@@ -48,28 +48,28 @@ FSL_TEST_FUNCTION(save_response) {
 
 
 FSL_TEST_FUNCTION(update_response) {
-    proxy::flush_cache();
-    FSL_CHECK_NOTHROW(proxy::save_entry(
+    fostlib::flush_cache();
+    FSL_CHECK_NOTHROW(fostlib::save_entry(
         fostlib::http::server::request("GET", "/"),
         fostlib::http::user_agent::response("GET",
             fostlib::url("http://example.com/"), 200,
             boost::make_shared<fostlib::binary_body>())));
-    const boost::filesystem::wpath file(proxy::update_entry(
+    const boost::filesystem::wpath file(fostlib::update_entry(
         "d3c8eae015367cfdcd581ddbef8fa58f",
         "d41d8cd98f00b204e9800998ecf8427e"));
-    std::shared_ptr<fostlib::jsondb> fdb(
-        proxy::cache_db(fostlib::string("d3")));
+    boost::shared_ptr<fostlib::jsondb> fdb(
+        fostlib::cache_db(fostlib::string("d3")));
 }
 
 
 FSL_TEST_FUNCTION(retrieve_response) {
-    proxy::flush_cache();
-    FSL_CHECK_NOTHROW(proxy::save_entry(
+    fostlib::flush_cache();
+    FSL_CHECK_NOTHROW(fostlib::save_entry(
         fostlib::http::server::request("GET", "/"),
         fostlib::http::user_agent::response("GET",
             fostlib::url("http://example.com/"), 200,
             boost::make_shared<fostlib::binary_body>())));
-    fostlib::json entry(proxy::db_entry(
+    fostlib::json entry(fostlib::db_entry(
         "d3c8eae015367cfdcd581ddbef8fa58f"));
     FSL_CHECK_NEQ(entry, fostlib::json());
     FSL_CHECK_EQ(entry["hash"],
@@ -78,13 +78,13 @@ FSL_TEST_FUNCTION(retrieve_response) {
 
 
 FSL_TEST_FUNCTION(cache_miss) {
-    proxy::flush_cache();
-    proxy::save_entry(
+    fostlib::flush_cache();
+    fostlib::save_entry(
         fostlib::http::server::request("GET", "/"),
         fostlib::http::user_agent::response("GET",
             fostlib::url("http://example.com/"), 200,
             boost::make_shared<fostlib::binary_body>()));
-    fostlib::json entry(proxy::db_entry(
+    fostlib::json entry(fostlib::db_entry(
         "00e39d220ff38421b6dd61a998975b28"));
     FSL_CHECK_EQ(entry, fostlib::json());
 }
--- Cpp/include/fost/http-cache.hpp
+++ Cpp/include/fost/http-cache.hpp
@@ -6,6 +6,11 @@
 */
 
 
+#ifndef FOST_HTTP_CACHE_HPP
+#define FOST_HTTP_CACHE_HPP
+#pragma once
+
+
 #include <fost/http>
 #include <fost/http.server.hpp>
 #include <fost/file>
@@ -16,7 +21,7 @@ namespace fostlib {
 
 
     /// Location for the cache data
-    extern const fostlib::setting<fostlib::string> c_cache_dir;
+    extern const setting<string> c_cache_dir;
 
 
     /// Return the root path for the cache
@@ -24,15 +29,15 @@ namespace fostlib {
 
 
     /// Return the cache address hash for the response
-    fostlib::string hash(const fostlib::http::user_agent::request &);
+    string hash(const http::user_agent::request &);
     /// Return the cache address hash for the response
-    fostlib::string hash(const fostlib::http::user_agent::response &);
+    string hash(const http::user_agent::response &);
 
     /// Return the variant hash for the set of headers
-    fostlib::string variant(const fostlib::mime::mime_headers &);
+    string variant(const mime::mime_headers &);
     /// Return the variant hash for the headers given a Vary header
-    fostlib::string variant(const fostlib::mime::mime_headers &,
-        const fostlib::string &vary);
+    string variant(const mime::mime_headers &,
+        const string &vary);
 
 
     /// Flush the entire cache
@@ -40,35 +45,37 @@ namespace fostlib {
 
 
     /// Returns the cache database (if we already know the root location)
-    std::shared_ptr<fostlib::jsondb> cache_db(
+    boost::shared_ptr<jsondb> cache_db(
         const boost::filesystem::wpath &,
-        const fostlib::nullable<fostlib::string> &subdb = fostlib::null);
+        const nullable<string> &subdb = null);
 
 
     /// Returns the cache database
-    inline std::shared_ptr<fostlib::jsondb> cache_db() {
+    inline boost::shared_ptr<jsondb> cache_db() {
         return cache_db(root());
     }
     /// Returns the file sub-database
-    inline std::shared_ptr<fostlib::jsondb> cache_db(
-        const fostlib::string &sdb
+    inline boost::shared_ptr<jsondb> cache_db(
+        const string &sdb
     ) {
         return cache_db(root(), sdb);
     }
 
 
     /// Returns the cache DB entry for the hash
-    fostlib::json db_entry(const fostlib::string &hash);
+    json db_entry(const string &hash);
 
 
     /// Save a response into the cache -- returns path to save body at
     boost::filesystem::wpath save_entry(
-        const fostlib::http::server::request &,
-        const fostlib::http::user_agent::response &);
+        const http::server::request &,
+        const http::user_agent::response &);
     /// Update an entry -- cache hit
     boost::filesystem::wpath update_entry(
-        const fostlib::string &h, const fostlib::string &vh);
+        const string &h, const string &vh);
 
 
 }
 
+
+#endif // FOST_HTTP_CACHE_HPP
