44633a88f0c4bb66ff0f684839ee2505d7ccaced
Kirit Sælensminde <k@kirit.com>
2014-11-26 17:25:52 +0700

    Add a test to make sure the single settting configuration finds the view.

---
+++ Cpp/fost-urlhandler/view-tests.cpp
@@ -0,0 +1,30 @@
+/*
+    Copyright 2014 Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-urlhandler.hpp"
+#include <fost/urlhandler>
+#include <fost/test>
+
+
+FSL_TEST_SUITE(view);
+
+
+FSL_TEST_FUNCTION(finds_view) {
+    fostlib::json configuration;
+    fostlib::insert(configuration, "view1", "view", "view2");
+    fostlib::insert(configuration, "view2", "view", "final.view");
+    fostlib::insert(configuration, "view2", "configuration", "config items");
+    const fostlib::setting<fostlib::json> views("view-tests.cpp",
+        fostlib::urlhandler::c_views, configuration);
+
+    std::pair<fostlib::string, fostlib::json> found(
+        fostlib::urlhandler::view::find_view("view1", fostlib::json()));
+    FSL_CHECK_EQ(found.first, "final.view");
+    FSL_CHECK_EQ(found.second, fostlib::json("config items"));
+}
+
