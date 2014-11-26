c55ef32da3ed17a40a7d1ff325d9fb71acccb393
Kirit Sælensminde <k@kirit.com>
2014-11-26 17:42:03 +0700

    Make sure we can have settings for individual views.

--- Cpp/fost-urlhandler/view-tests.cpp
+++ Cpp/fost-urlhandler/view-tests.cpp
@@ -28,3 +28,24 @@ FSL_TEST_FUNCTION(finds_view) {
     FSL_CHECK_EQ(found.second, fostlib::json("config items"));
 }
 
+
+FSL_TEST_FUNCTION(finds_view_in_other_configuration) {
+    fostlib::json configuration1;
+    fostlib::insert(configuration1, "view1", "view", "view2");
+    const fostlib::setting<fostlib::json> views("view-tests.cpp",
+        fostlib::urlhandler::c_views, configuration1);
+
+    fostlib::json configuration2;
+    fostlib::insert(configuration2, "view", "final.view");
+    fostlib::insert(configuration2, "configuration", "config items");
+    const fostlib::setting<fostlib::json> view2("view-tests.cpp",
+        fostlib::urlhandler::c_views.section(),
+        fostlib::urlhandler::c_views.name() + "/view2",
+        configuration2);
+
+    std::pair<fostlib::string, fostlib::json> found(
+        fostlib::urlhandler::view::find_view("view1", fostlib::json()));
+    FSL_CHECK_EQ(found.first, "final.view");
+    FSL_CHECK_EQ(found.second, fostlib::json("config items"));
+}
+
