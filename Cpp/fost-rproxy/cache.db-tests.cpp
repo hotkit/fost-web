8572bfe65e3f1cdd7bb0b1dd9966a70e9355101c
Kirit Sælensminde <k@kirit.com>
2014-12-05 10:07:03 +0700

    Fix some string literals that need to be wide.

--- Cpp/fost-rproxy/cache.db-tests.cpp
+++ Cpp/fost-rproxy/cache.db-tests.cpp
@@ -37,17 +37,17 @@ FSL_TEST_FUNCTION(save_response) {
         fostlib::url("http://example.com/"), 200, empty_body());
     FSL_CHECK_EQ(
         fostlib::save_entry(req, r),
-        boost::filesystem::wpath("d3") /
+        boost::filesystem::wpath(L"d3") /
             L"c8eae015367cfdcd581ddbef8fa58f"
             L"-d41d8cd98f00b204e9800998ecf8427e");
     boost::shared_ptr<fostlib::jsondb> db(fostlib::cache_db());
     fostlib::jsondb::local trans(*db);
     FSL_CHECK_EQ(trans["file-db"]["d3"]["db"],
         fostlib::coerce<fostlib::json>(
-            boost::filesystem::wpath("d3") / L"file-db.json"));
+            boost::filesystem::wpath(L"d3") / L"file-db.json"));
     FSL_CHECK_EQ(
         fostlib::save_entry(req, r),
-        boost::filesystem::wpath("d3") /
+        boost::filesystem::wpath(L"d3") /
             L"c8eae015367cfdcd581ddbef8fa58f"
             L"-d41d8cd98f00b204e9800998ecf8427e");
 }
