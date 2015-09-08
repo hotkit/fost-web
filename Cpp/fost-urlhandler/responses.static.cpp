7adfa0c4ae6d91184c36566c0fa5ed3ade189fc1
Kirit Sælensminde <k@kirit.com>
2015-09-08 10:29:25 +0700

    Add the ability for the static view to handle DELETE requests

--- Cpp/fost-urlhandler/responses.static.cpp
+++ Cpp/fost-urlhandler/responses.static.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2011-2012 Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2011-2015 Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -8,6 +8,7 @@
 
 #include "fost-urlhandler.hpp"
 #include <fost/urlhandler.hpp>
+#include <fost/push_back>
 #include <fost/unicode>
 
 
@@ -15,6 +16,11 @@ const class response_static : public fostlib::urlhandler::view {
     public:
         response_static()
         : view("fost.static") {
+        }
+
+        static bool allow_delete(const fostlib::json &conf) {
+            return conf.has_key("verbs") &&
+                fostlib::coerce<fostlib::nullable<bool>>(conf["verbs"]["DELETE"]).value(false);
         }
 
         std::pair<boost::shared_ptr<fostlib::mime>, int> operator () (
@@ -31,10 +37,27 @@ const class response_static : public fostlib::urlhandler::view {
                 filename /= L"index.html";
             if ( !boost::filesystem::exists(filename) )
                 return fostlib::urlhandler::response_404(fostlib::json(), path, req, host);
-            boost::shared_ptr<fostlib::mime> response(
-                    new fostlib::file_body(filename, fostlib::mime::mime_headers(),
-                        fostlib::urlhandler::mime_type(filename)));
-            return std::make_pair(response, 200);
+            if ( req.method() == "GET" || req.method() == "HEAD" ) {
+                boost::shared_ptr<fostlib::mime> response(
+                        new fostlib::file_body(filename, fostlib::mime::mime_headers(),
+                            fostlib::urlhandler::mime_type(filename)));
+                return std::make_pair(response, 200);
+            } else if ( allow_delete(configuration) && req.method() == "DELETE" ) {
+                boost::filesystem::remove(filename);
+                boost::shared_ptr<fostlib::mime> response(
+                        new fostlib::text_body(
+                            L"<html><head><title>Resource deleted</title></head>"
+                                L"<body><h1>Resource deleted</h1></body></html>",
+                            fostlib::mime::mime_headers(), L"text/html" ));
+                return std::make_pair(response, 200);
+            } else {
+                fostlib::json c405;
+                fostlib::push_back(c405, "allow", "GET");
+                fostlib::push_back(c405, "allow", "HEAD");
+                if ( allow_delete(configuration) )
+                    fostlib::push_back(c405, "allow", "DELETE");
+                return fostlib::urlhandler::response_405(c405, path, req, host);
+            }
         }
 } c_response_static;
 
