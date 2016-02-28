d5d1d65a11bfc1df94a5446cd118e0b4c5ac289a
Kirit Sælensminde <k@kirit.com>
2016-02-28 12:20:11 +0700

    Allow multiple configuration files to be loaded and improve logging.

--- Cpp/fost-webserver/webserver.cpp
+++ Cpp/fost-webserver/webserver.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2014 Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2008-2016 Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -18,25 +18,47 @@ using namespace fostlib;
 
 
 namespace {
-    setting< string > c_host("webserver.cpp",
+    const setting<string> c_host("webserver.cpp",
         "webserver", "Bind to", "localhost");
-    setting< int > c_port("webserver.cpp",
+    const setting<int> c_port("webserver.cpp",
         "webserver", "Port", 8001);
-    setting< string > c_mime("webserver.cpp",
+    const setting<string> c_mime("webserver.cpp",
         "webserver", "MIME types", "Configuration/mime-types.json");
-    setting< json > c_load("webserver.cpp",
+    const setting<json> c_load("webserver.cpp",
         "webserver", "Load", json::array_t());
+
+    const setting<json> c_logger("webserver.cpp",
+        "webserver", "logging", fostlib::json(), true);
+    // Take out the Fost logger configuration so we don't end up with both
+    const setting<json> c_fost_logger("webserver.cpp",
+        "webserver", "Logging sinks", fostlib::json::parse("{\"sinks\":[]}"));
 }
 
 
 FSL_MAIN(
     L"webserver",
-    L"Threaded HTTP server\nCopyright (c) 2002-2011, Felspar Co. Ltd."
+    L"Threaded HTTP server\nCopyright (C) 2002-2016, Felspar Co. Ltd."
 )( fostlib::ostream &o, fostlib::arguments &args ) {
+    // Load the configuration files we've been given on the command line
+    std::vector<fostlib::settings> configuration;
+    configuration.reserve(args.size());
+    for ( std::size_t arg{1}; arg != args.size(); ++arg ) {
+        auto filename = fostlib::coerce<boost::filesystem::path>(args[arg].value());
+        o << "Loading config " << filename << std::endl;
+        configuration.emplace_back(std::move(filename));
+    }
+
     // Process command line arguments last
     args.commandSwitch("p", c_port.section(), c_port.name());
     args.commandSwitch("h", c_host.section(), c_host.name());
     args.commandSwitch("m", c_mime.section(), c_mime.name());
+
+    // Set up the logging options
+    std::unique_ptr<fostlib::log::global_sink_configuration> loggers;
+    if ( not c_logger.value().isnull() && c_logger.value().has_key("sinks") ) {
+        loggers =
+            std::make_unique<fostlib::log::global_sink_configuration>(c_logger.value());
+    }
 
     // Load MIME types
     urlhandler::load_mime_configuration(c_mime.value());
@@ -54,7 +76,7 @@ FSL_MAIN(
         L"http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
 
     // Service requests
-    server( urlhandler::service );
+    server(urlhandler::service);
 
     // It will never get this far
     return 0;
