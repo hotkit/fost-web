7ff499d401edd0b331b7637fde09e8b1eb102257
Kirit Sælensminde <k@kirit.com>
2016-09-23 11:42:51 +0700

    Removed the old `router` function as it's been replaced by `view::execute`.

--- Cpp/include/fost/urlhandler.hpp
+++ Cpp/include/fost/urlhandler.hpp
@@ -37,13 +37,6 @@ namespace fostlib {
         FOST_URLHANDLER_DECLSPEC
             bool service( fostlib::http::server::request &req );
 
-        /// The routing part of the web service
-        FOST_URLHANDLER_DECLSPEC
-            std::pair<boost::shared_ptr<fostlib::mime>, int >
-                router(const fostlib::host &requested_host,
-                    const fostlib::string &view_name,
-                    fostlib::http::server::request &req);
-
         /// Load MIME configuration data from the specified file
         FOST_URLHANDLER_DECLSPEC
             void load_mime_configuration(const fostlib::string &filename);
