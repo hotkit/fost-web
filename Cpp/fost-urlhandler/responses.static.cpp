a70090fee9054abc047b89dfb8622c8197b738e1
Kirit Sælensminde <kirit@kirit.com>
2019-04-11 08:41:20 +0700

    Start to refactor the static file handling.

--- Cpp/fost-urlhandler/responses.static.cpp
+++ Cpp/fost-urlhandler/responses.static.cpp
@@ -12,58 +12,106 @@
 #include <fost/unicode>
 
 
-const class response_static : public fostlib::urlhandler::view {
-  public:
-    response_static() : view("fost.static") {}
+namespace {
 
-    static bool allow_delete(const fostlib::json &conf) {
-        return conf.has_key("verbs")
-                && fostlib::coerce<fostlib::nullable<bool>>(
-                           conf["verbs"]["DELETE"])
-                           .value_or(false);
-    }
 
-    std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
-            const fostlib::json &configuration,
-            const fostlib::string &path,
-            fostlib::http::server::request &req,
-            const fostlib::host &host) const {
-        fostlib::fs::path root(
-                fostlib::coerce<fostlib::fs::path>(configuration["root"]));
-        fostlib::fs::path filename =
-                root / fostlib::coerce<fostlib::fs::path>(path);
-        if (fostlib::fs::is_directory(filename)) {
+    const class directory : public fostlib::urlhandler::view {
+      public:
+        directory() : view{"fost.static.directory"} {}
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
+                const fostlib::json &configuration,
+                const fostlib::string &path,
+                fostlib::http::server::request &req,
+                const fostlib::host &host) const {
+            fostlib::fs::path root(
+                    fostlib::coerce<fostlib::fs::path>(configuration["root"]));
+            fostlib::fs::path filename =
+                    root / fostlib::coerce<fostlib::fs::path>(path);
+            if (fostlib::fs::is_directory(filename)) {
+                return serve_directory(configuration, filename, req);
+            } else {
+                throw fostlib::exceptions::not_implemented(
+                        __PRETTY_FUNCTION__,
+                        "Told to serve a directory, but it turned out to be a "
+                        "file");
+            }
+        }
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> serve_directory(
+                const fostlib::json &configuration,
+                const fostlib::fs::path &dirname,
+                fostlib::http::server::request &req) const {
+            auto filename = dirname;
             if (configuration.has_key("index")) {
                 filename /= fostlib::coerce<fostlib::fs::path>(
                         configuration["index"]);
             } else {
                 filename /= "index.html";
             }
+            if (fostlib::fs::exists(filename)) {
+                return fostlib::urlhandler::serve_file(
+                        configuration, req, filename);
+            }
+            /// There is no index file in this directory
+            throw fostlib::exceptions::not_implemented(__PRETTY_FUNCTION__);
         }
-        if (!fostlib::fs::exists(filename)) {
-            return fostlib::urlhandler::response_404(
-                    fostlib::json(), path, req, host);
+    } c_directory;
+
+
+    const class response_static : public fostlib::urlhandler::view {
+      public:
+        response_static() : view("fost.static") {}
+
+        static bool allow_delete(const fostlib::json &conf) {
+            return conf.has_key("verbs")
+                    && fostlib::coerce<fostlib::nullable<bool>>(
+                               conf["verbs"]["DELETE"])
+                               .value_or(false);
         }
-        if (req.method() == "GET" || req.method() == "HEAD") {
-            return fostlib::urlhandler::serve_file(
-                    configuration, req, filename);
-        } else if (allow_delete(configuration) && req.method() == "DELETE") {
-            fostlib::fs::remove(filename);
-            boost::shared_ptr<fostlib::mime> response(new fostlib::text_body(
-                    L"<html><head><title>Resource deleted</title></head>"
-                    L"<body><h1>Resource deleted</h1></body></html>",
-                    fostlib::mime::mime_headers(), L"text/html"));
-            return std::make_pair(response, 200);
-        } else {
-            fostlib::json c405;
-            fostlib::push_back(c405, "allow", "GET");
-            fostlib::push_back(c405, "allow", "HEAD");
-            if (allow_delete(configuration))
-                fostlib::push_back(c405, "allow", "DELETE");
-            return fostlib::urlhandler::response_405(c405, path, req, host);
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
+                const fostlib::json &configuration,
+                const fostlib::string &path,
+                fostlib::http::server::request &req,
+                const fostlib::host &host) const {
+            fostlib::fs::path root(
+                    fostlib::coerce<fostlib::fs::path>(configuration["root"]));
+            fostlib::fs::path filename =
+                    root / fostlib::coerce<fostlib::fs::path>(path);
+            if (fostlib::fs::is_directory(filename)) {
+                return c_directory.serve_directory(
+                        configuration, filename, req);
+            } else {
+                if (not fostlib::fs::exists(filename)) {
+                    return fostlib::urlhandler::response_404(
+                            fostlib::json(), path, req, host);
+                }
+            }
+            if (req.method() == "GET" || req.method() == "HEAD") {
+                return fostlib::urlhandler::serve_file(
+                        configuration, req, filename);
+            } else if (allow_delete(configuration) && req.method() == "DELETE") {
+                fostlib::fs::remove(filename);
+                boost::shared_ptr<fostlib::mime> response(new fostlib::text_body(
+                        L"<html><head><title>Resource deleted</title></head>"
+                        L"<body><h1>Resource deleted</h1></body></html>",
+                        fostlib::mime::mime_headers(), L"text/html"));
+                return std::make_pair(response, 200);
+            } else {
+                fostlib::json c405;
+                fostlib::push_back(c405, "allow", "GET");
+                fostlib::push_back(c405, "allow", "HEAD");
+                if (allow_delete(configuration))
+                    fostlib::push_back(c405, "allow", "DELETE");
+                return fostlib::urlhandler::response_405(c405, path, req, host);
+            }
         }
-    }
-} c_response_static;
+    } c_response_static;
+
+
+}
+
 
 const fostlib::urlhandler::view &fostlib::urlhandler::static_server =
         c_response_static;
