d8dd82a064446296e103eb39f6ef3b67bcd47231
Kirit Sælensminde <kirit@kirit.com>
2019-04-11 10:24:15 +0700

    Add tests for basic directory file serving.

--- Cpp/fost-urlhandler/responses.static.tests.cpp
+++ Cpp/fost-urlhandler/responses.static.tests.cpp
@@ -11,15 +11,34 @@
 
 
 namespace {
+
+
     fostlib::setting<fostlib::string> const c_mime_html{
             "responses.static.tests.cpp", "MIME", ".html", "text/html"};
 
     fostlib::fs::path const c_file_root{fostlib::unique_filename()};
 
-    void setup() {
+
+    fostlib::json setup() {
         fostlib::fs::create_directory(c_file_root);
         fostlib::utf::save_file(c_file_root / "index.html", "html");
+        fostlib::utf::save_file(c_file_root / "index.txt", "text");
+        fostlib::json conf;
+        fostlib::insert(conf, "root", c_file_root);
+        return conf;
     }
+
+
+    fostlib::string content(const fostlib::mime &body) {
+        fostlib::string text;
+        for (auto part : body) {
+            text += std::string{reinterpret_cast<char const *>(part.first),
+                                reinterpret_cast<char const *>(part.second)};
+        }
+        return text;
+    }
+
+
 }
 
 
@@ -27,12 +46,34 @@ FSL_TEST_SUITE(static_files);
 
 
 FSL_TEST_FUNCTION(file) {
-    setup();
-    fostlib::json conf;
-    fostlib::insert(conf, "root", c_file_root);
+    auto conf = setup();
     fostlib::http::server::request req("GET", "/index.html");
     auto [resp, status] = fostlib::urlhandler::static_server(
             conf, "index.html", req, fostlib::host{});
     FSL_CHECK_EQ(status, 200);
     FSL_CHECK_EQ(resp->headers()["Content-Type"].value(), "text/html");
+    FSL_CHECK_EQ(content(*resp), "html");
+}
+
+
+FSL_TEST_FUNCTION(default_file) {
+    auto conf = setup();
+    fostlib::http::server::request req("GET", "/");
+    auto [resp, status] =
+            fostlib::urlhandler::static_server(conf, "", req, fostlib::host{});
+    FSL_CHECK_EQ(status, 200);
+    FSL_CHECK_EQ(resp->headers()["Content-Type"].value(), "text/html");
+    FSL_CHECK_EQ(content(*resp), "html");
+}
+
+
+FSL_TEST_FUNCTION(custom_default_file) {
+    auto conf = setup();
+    fostlib::insert(conf, "index", "index.txt");
+    fostlib::http::server::request req("GET", "/");
+    auto [resp, status] =
+            fostlib::urlhandler::static_server(conf, "", req, fostlib::host{});
+    FSL_CHECK_EQ(status, 200);
+    FSL_CHECK_EQ(resp->headers()["Content-Type"].value(), "text/plain");
+    FSL_CHECK_EQ(content(*resp), "text");
 }
