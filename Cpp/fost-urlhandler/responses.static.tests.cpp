18ddb8372f94cff851f2c9722c9ea41dad28ae35
Kirit Sælensminde <kirit@kirit.com>
2019-04-11 10:16:33 +0700

    Test that a specific file is returned.

--- Cpp/fost-urlhandler/responses.static.tests.cpp
+++ Cpp/fost-urlhandler/responses.static.tests.cpp
@@ -10,7 +10,29 @@
 #include <fost/test>
 
 
+namespace {
+    fostlib::setting<fostlib::string> const c_mime_html{
+            "responses.static.tests.cpp", "MIME", ".html", "text/html"};
+
+    fostlib::fs::path const c_file_root{fostlib::unique_filename()};
+
+    void setup() {
+        fostlib::fs::create_directory(c_file_root);
+        fostlib::utf::save_file(c_file_root / "index.html", "html");
+    }
+}
+
+
 FSL_TEST_SUITE(static_files);
 
 
-FSL_TEST_FUNCTION(file) {}
+FSL_TEST_FUNCTION(file) {
+    setup();
+    fostlib::json conf;
+    fostlib::insert(conf, "root", c_file_root);
+    fostlib::http::server::request req("GET", "/index.html");
+    auto [resp, status] = fostlib::urlhandler::static_server(
+            conf, "index.html", req, fostlib::host{});
+    FSL_CHECK_EQ(status, 200);
+    FSL_CHECK_EQ(resp->headers()["Content-Type"].value(), "text/html");
+}
