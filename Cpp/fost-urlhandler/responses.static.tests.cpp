6331df5d2e6cb0b6c5eee67ceb0e3faa3e0d7e3a
Kirit Sælensminde <kirit@kirit.com>
2019-04-11 08:41:38 +0700

    We are going to need much better tests now this is becoming more complex.

---
+++ Cpp/fost-urlhandler/responses.static.tests.cpp
@@ -0,0 +1,16 @@
+/**
+    Copyright 2019 Felspar Co Ltd. <http://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+ */
+
+
+#include <fost/urlhandler>
+#include <fost/test>
+
+
+FSL_TEST_SUITE(static_files);
+
+
+FSL_TEST_FUNCTION(file) {}
