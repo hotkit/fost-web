b2a185418d67bf8e7a90a8556a3e1b4fc8b37ef5
Kirit Sælensminde <kirit@kirit.com>
2019-11-06 12:45:04 +0700

    Add tests for current transparent proxy.

--- Cpp/fost-urlhandler-test/web-proxy.cpp
+++ Cpp/fost-urlhandler-test/web-proxy.cpp
@@ -6,19 +6,50 @@
  */
 
 
+#include <fost/insert>
 #include <fost/urlhandler>
 #include <fost/test>
+
+
+namespace {
+    auto proxy_config(f5::u8view const upstream) {
+        fostlib::json conf;
+        insert(conf, "view", "fost.proxy.transparent");
+        insert(conf, "configuration", "base", upstream);
+        return conf;
+    }
+}
 
 
 FSL_TEST_SUITE(web_proxy);
 
 
 /// Connect to an upstream server over HTTP
-FSL_TEST_FUNCTION(http) {}
+FSL_TEST_FUNCTION(http) {
+    fostlib::http::server::request req;
+    req.headers().add("Host", "neverssl.com");
+    auto const config = proxy_config("http://neverssl.com/");
+    auto const [response, status] = fostlib::urlhandler::view::execute(
+            config, "/", req, fostlib::host{"neverssl.com"});
+    FSL_CHECK_EQ(status, 200);
+    FSL_CHECK(
+            fostlib::string{response->body_as_string()}.find("NeverSSL")
+            != fostlib::string::npos);
+}
 
 
 /// Connect to an upstream server using TLS
-FSL_TEST_FUNCTION(https) {}
+FSL_TEST_FUNCTION(https) {
+    fostlib::http::server::request req;
+    req.headers().add("Host", "sha256.badssl.com");
+    auto const config = proxy_config("https://sha256.badssl.com/");
+    auto const [response, status] = fostlib::urlhandler::view::execute(
+            config, "/", req, fostlib::host{"sha256.badssl.com"});
+    FSL_CHECK_EQ(status, 200);
+    FSL_CHECK(
+            fostlib::string{response->body_as_string()}.find("sha256")
+            != fostlib::string::npos);
+}
 
 
 /// Connect to an upstream server where the host header
