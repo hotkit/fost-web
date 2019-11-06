43273268a89f15051d1e0a80af22dcbc2903ee7c
Kirit Sælensminde <kirit@kirit.com>
2019-11-06 13:33:12 +0700

    Split out base proxy class and allow cuustomisation of the upstream URL.

--- Cpp/fost-urlhandler/responses.proxy.cpp
+++ Cpp/fost-urlhandler/responses.proxy.cpp
@@ -15,46 +15,63 @@
 namespace {
 
 
-    class proxy_base : public fostlib::urlhandler::view {
-      protected:
-        proxy_base(f5::u8view const n) : view(n) {}
-
-
-        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
-                const fostlib::json &configuration,
-                const fostlib::string &path,
-                fostlib::http::server::request &request,
-                const fostlib::host &host) const override {
-            fostlib::timer time;
-            auto info = fostlib::log::info(fostlib::c_fost_web_urlhandler);
-            info("id", fostlib::guid())(
-                    "request", "path", "full", request.file_spec())(
-                    "request", "path", "outstanding", path);
-
-            fostlib::url base(
-                    fostlib::coerce<fostlib::string>(configuration["base"]));
-            fostlib::url location(base, path);
-            location.query(request.query_string());
-            info("proxy", "url", location);
-            info("proxy", "headers", request.data()->headers());
-
-            fostlib::http::user_agent ua(base);
-            fostlib::http::user_agent::request proxy(
-                    request.method(), location, request.data());
-            auto response = ua(proxy);
-            auto body{response->body()};
-            info("repsonse", "status", response->status());
-            info("time", time.seconds());
-
-            return std::make_pair(body, response->status());
-        }
-    };
-
-
-    const class transparent final : public proxy_base {
+    /**
+        The transparent proxy doesn't change the request or the
+        server response. It's used in situations where the web server
+        is intended to intercept a request and route it on as if it had
+        handled the request itself. This is normally done with the co-operation
+        of the upstream host.
+     */
+    const class transparent final : public fostlib::web_proxy::base {
       public:
-        transparent() : proxy_base("fost.proxy.transparent") {}
+        transparent() : base("fost.proxy.transparent") {}
+
     } c_transparent;
 
 
+}
+
+
+/**
+ * ## `fostlib::web_proxy::base`
+ */
+
+
+std::pair<boost::shared_ptr<fostlib::mime>, int>
+        fostlib::web_proxy::base::operator()(
+                json const &configuration,
+                string const &path,
+                http::server::request &request,
+                host const &host) const {
+    timer time;
+    auto info = log::info(fostlib::c_fost_web_urlhandler);
+    info("id", fostlib::guid());
+    info("request", "path", "full", request.file_spec());
+    info("request", "path", "outstanding", path);
+
+    url const base{coerce<string>(configuration["base"])};
+    auto const location = upstream(configuration, base, path, request, host);
+    info("proxy", "url", location);
+    info("proxy", "headers", request.data()->headers());
+
+    http::user_agent ua(base);
+    http::user_agent::request proxy(request.method(), location, request.data());
+    auto response = ua(proxy);
+    auto body{response->body()};
+    info("repsonse", "status", response->status());
+    info("time", time.seconds());
+
+    return std::make_pair(body, response->status());
+}
+
+
+fostlib::url fostlib::web_proxy::base::upstream(
+        json const &configuration,
+        url const &base,
+        string const &path,
+        http::server::request &request,
+        host const &host) const {
+    url location{base, path};
+    location.query(request.query_string());
+    return location;
 }
--- Cpp/include/fost/web-proxy.hpp
+++ Cpp/include/fost/web-proxy.hpp
@@ -8,7 +8,36 @@
 
 #pragma once
 
+
 #include <fost/urlhandler.hpp>
 
 
-namespace fostlib {}
+namespace fostlib::web_proxy {
+
+
+    /// ## `base`
+    /**
+        Abstract class that defines the basics of the different proxy types.
+    */
+    class base : public fostlib::urlhandler::view {
+      protected:
+        base(f5::u8view const n) : view(n) {}
+
+        /// Customisation point for determining the new URL that is to be used
+        virtual url upstream(
+                json const &configuration,
+                url const &base,
+                string const &path,
+                http::server::request &request,
+                host const &host) const;
+
+      private:
+        std::pair<boost::shared_ptr<mime>, int> operator()(
+                json const &configuration,
+                string const &path,
+                http::server::request &request,
+                host const &host) const override;
+    };
+
+
+}
